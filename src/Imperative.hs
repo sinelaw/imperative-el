d39e14f8a72e227ae68314b7612892398b6601e8
Noam Lewis <jones.noamle@gmail.com>
2010-11-24 10:17:38 +0200

    second

--- src/Imperative.hs
+++ src/Imperative.hs
@@ -2,13 +2,17 @@
 module Main where
 
 import Data.Typeable
+import Data.List(intersperse)
 
 data Variable a = Variable String
                 deriving (Show)
 
+data Tuple a b = Tuple a b
+               deriving (Show, Typeable)
+
 data Expr a where
   Literal    :: Show a => a -> Expr a
-  Tuple      :: (Show a, Show b) => Expr a -> Expr b -> Expr (a,b)
+  TupleExp   :: (Show a, Show b) => Expr a -> Expr b -> Expr (Tuple a b)
   VarExp     :: Variable a -> Expr a
   Assignment :: Variable a -> Expr a -> Expr a
   deriving (Typeable)
@@ -16,35 +20,28 @@ data Expr a where
 --struct (exprA:(exprB:[])) = Tuple exprA exprB
 --struct (expr:exprs)       = Tuple expr (struct exprs)
   
+
 instance Show a => Show (Expr a) where
   show (Literal l) = "Literal " ++ show l
   show (Assignment var val) = show var ++ " = " ++ show val
   show (VarExp v) = show v
-  show (Tuple a b) = "(" ++ show a ++ ", " ++ show b ++ ")"
+  show (TupleExp a b) = "(" ++ show a ++ ", " ++ show b ++ ")"
 
-class DSLType a where
-  showType :: Expr a -> String
 
-instance DSLType Integer where showType _ = "Integer"
-instance DSLType Double where  showType _ = "Double"    
-instance DSLType String where  showType _ = "String"
+showType :: (TyCon, [TypeRep]) -> String
+showType (tCon, []) = show tCon
+showType (tCon, tReps) = concat ([show tCon, "<", tRepStrs, ">"])
+  where tRepStrs = concat . intersperse "," . map (showType . splitTyConApp) $ tReps
 
--- need something that does:
--- instance (DSLType a, DSLType b) => DSLType (a,b) where  
---   showType _ = "Tuple<" ++ showType a ++ ", " ++ showType b ++ ">"
-
--- Doesn't work! vEx's type is not determined for some reason
 l = Literal (3::Integer)
 v = Variable "v"
-vEx = VarExp v
-a = Assignment v vEx
-b = Assignment v (Tuple l l)
+b = Assignment v (TupleExp l l)
       
 main :: IO ()
 main = do
   print b
-  print (showType b)
---  print (showType vEx)
+  print (typeOf b)
+  print (showType (splitTyConApp . typeOf $ b))
   return ()
 
   
